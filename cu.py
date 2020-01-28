f1498073c9bff634294ae026aee5687403a60550
Irakli <i_lekishvili@cu.edu.ge>
2020-01-28 15:11:23 +0400

    automated cu marks checker

---
+++ cu.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# set up
+chromeDriver = '.\\chromeDrivers\\chromeDriver'
+chromeDriver18 = '.\\chromeDrivers\\chromeDriver18'
+chromeDriver19 = '.\\chromeDrivers\\chromeDriver19'
+
+us = str(input('username: '))
+ps = str(input('password: '))
+
+
+# code
+class CuBot:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        try:
+            self.bot = webdriver.Chrome(chromeDriver)
+        except:
+            try:
+                self.bot = webdriver.Chrome(chromeDriver19)
+            except:
+                self.bot = webdriver.Chrome(chromeDriver18)
+
+    def login(self):
+        bot = self.bot
+        bot.get('https:programs.cu.edu.ge/cu/loginStud')
+        nick = bot.find_element_by_name('username')
+        passwd = bot.find_element_by_name('password')
+        nick.send_keys(self.username)
+        passwd.send_keys(self.password)
+        passwd.send_keys(Keys.RETURN)
+
+    def nishnebi(self):
+        bot = self.bot
+        bot.get('https://programs.cu.edu.ge/students/gpa.php')
+
+
+ir = CuBot(us, ps)
+ir.login()
+ir.nishnebi()
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Only works on windows
+* ### To run you should have new versions of chrome 78 - 79 - 80
+    you can check it by opening new tab and writing chrome://version
+
+1. Download exe version to run right away
+2. If you want to run python version you need also install selenium
+with pip install selenium
